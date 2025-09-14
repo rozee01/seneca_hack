becca38d34a09ec13b4039b7b31ce57f13df4493
jihenmk <jihenmkhinini4070@flexsolvfrance.com>
2025-09-14 10:37:30 +0100

    logo

--- sportpulse-dashboard/src/components/Header/Header.js
+++ sportpulse-dashboard/src/components/Header/Header.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { MdSportsBaseball } from "react-icons/md";
+import logo from "../../assets/logo.png";
+
 const Header = () => {
   return (
     <header className="flex items-center justify-between whitespace-nowrap border-b border-solid border-b-[#283039] px-10 py-3 bg-gradient-to-r from-[#181C20] via-[#23272f] to-[#181C20] shadow-lg py-6">
       <div className="flex items-center gap-10">
         <div className="flex items-center gap-3 text-white">
-          <MdSportsBaseball size={30} />
-          <h2 className="text-white text-3xl font-extrabold leading-tight tracking-tighter drop-shadow">
-            Sportpulse
-          </h2>
+          <img
+            src={logo}
+            alt="Sportpulse Logo"
+            className="h-20 w-50 object-contain"
+          />
         </div>
       </div>
     </header>
